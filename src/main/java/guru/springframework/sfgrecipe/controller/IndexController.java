fec36bfc38e7f6c7ed547c4b228f8bff3bf5d1c0
dboeckli <Big.fat.cow-2010>
2021-02-02 21:02:28 +0100

    implement finders in repositories

--- src/main/java/guru/springframework/sfgrecipe/controller/IndexController.java
+++ src/main/java/guru/springframework/sfgrecipe/controller/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.sfgrecipe.controller;
 
+import guru.springframework.sfgrecipe.model.Category;
+import guru.springframework.sfgrecipe.model.UnitOfMeasure;
+import guru.springframework.sfgrecipe.repository.CategoryRepository;
+import guru.springframework.sfgrecipe.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category is: " + category.get().getId());
+        System.out.println("Unit Of Measure is: " + unitOfMeasure.get().getId());
+
         return "index";
     }
 
--- src/main/java/guru/springframework/sfgrecipe/repository/CategoryRepository.java
+++ src/main/java/guru/springframework/sfgrecipe/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.sfgrecipe.repository;
 import guru.springframework.sfgrecipe.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
